a05b853b4d481a23bc4d7ca458b94993876ca511
Roman Shveytser <truelive4@gmail.com>
2022-08-20 23:57:45 +0300

    introducing v2 site with npm

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+    // webpack folder’s entry js — excluded from jekll’s build process.
+    mode: "development",
+    entry: "./webpack/entry.js",
+    output: {
+      // we’re going to put the generated file in the assets folder so jekyll will grab it.
+      // if using GitHub Pages, use the following:
+      // path: "assets/javascripts"
+      path: __dirname + "/src/assets/js/",
+      filename: "bundle.js"
+    },
+    module: {
+    rules: [
+      { test: /\.css$/, use: ["style-loader", "css-loader"] },
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        loader: "babel-loader", // "babel-loader" is also a legal name to reference
+        options: {
+          presets: ["@babel/react", "@babel/preset-env"]
+        }
+      },
+      {
+        test: require.resolve('jquery'),
+        loader: 'expose-loader',
+        options: {
+          exposes: ['jQuery', '$']
+        }
+      }
+      ]
+    }
+  };
---
+++ webpack/entry.js
@@ -0,0 +1,6 @@
+var $  = require( "jquery");
+require( "jquery-ui");
+require('jquery-ui/themes/base/core.css');
+require('jquery-ui/themes/base/theme.css');
+require('jquery-ui/themes/base/datepicker.css');
+require('jquery-ui/ui/widgets/datepicker');
